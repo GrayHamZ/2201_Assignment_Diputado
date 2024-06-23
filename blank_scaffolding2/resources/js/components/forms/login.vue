81f77d7ae48de59874ee8111b4676847be920533
GrayHamZ <janritch@gmail.com>
2024-06-23 23:06:08 +0800

    first commit

---
+++ blank_scaffolding2/resources/js/components/forms/login.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <b-table striped hover :items="items" :fields="fields"></b-table>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        items: null,
+        fields: ['id', 'USERNAME']
+      }
+    },
+    methods:{
+      get_accounts(){
+        axios.get('/get_accounts').then(response=>{
+          console.log(response.data);
+          this.items = response.data;
+        }).catch(error=>{
+          console.log(error.data);
+        })
+      },
+      register() {
+        axios.post('/register/registration',this.form).then(response=>{
+          console.log(response.data.email);
+          console.log(response.data.password);
+        }).catch(error=>{
+          console.log(error.data);
+          this.loading = false;
+        })
+      }
+    },
+    beforeMount(){
+      this.get_accounts();
+    }
+  }
+</script>
